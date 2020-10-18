71f4c4abaefb89fdaa77a07c515b5fcffe5abc88
z5255395 <z5255395@ad.unsw.edu.au>
2020-10-18 13:49:56 +1100

    Made function

--- EMACD.py
+++ EMACD.py
@@ -6,45 +6,67 @@ import math
 data = pd.read_csv('data.csv')
 
 
-shortTermWeights = []
-longTermWeights = []
-
-shortTermLength = 12
-LongTermLength = 26
-expFactor = 0.2
-
-shortMovingAverage = 0
-longMovingAverage = 0
+def exponentialMovingAverage(expFactor, shortTermLength, LongTermLength):
+    shortTermWeights = []
+    longTermWeights = []
 
 
-i = 0
 
-while i < LongTermLength:
-    if( i < shortTermLength):
-        shortTermWeights.append(math.exp(expFactor * i))
-    longTermWeights.append(math.exp(expFactor * i))
-    i += 1
+    workingCapital = 1000000
+    cashAvailable = workingCapital
 
-shortTermTotalWeight = sum(shortTermWeights)
-longTermTotalWeight = sum(longTermWeights)
-
-i = 0
-while i < LongTermLength:
-    if( i < shortTermLength):
-        shortTermWeights[i] = (shortTermWeights[i] / shortTermTotalWeight)
-    longTermWeights[i] = (longTermWeights[i] / longTermTotalWeight)
-    i += 1 
+    shortMovingAverage = 0
+    longMovingAverage = 0
 
 
-i = LongTermLength
-prices = data['Price'].to_list()
-while i < len(prices):
-    longMovingAverage = (np.dot(prices[i - LongTermLength:i], longTermWeights))
-    shortMovingAverage = (np.dot(prices[i-shortTermLength:i], shortTermWeights))
-    difference = shortMovingAverage - longMovingAverage
-    if( difference >  0):
-        print('Buy on: ' + data['Date'][i])
-    elif( difference <  0):
-        print('Sell on: ' + data['Date'][i])
-    i += 1
+    i = 0
 
+    while i < LongTermLength:
+        if( i < shortTermLength):
+            shortTermWeights.append(math.exp(expFactor * i))
+        longTermWeights.append(math.exp(expFactor * i))
+        i += 1
+
+    shortTermTotalWeight = sum(shortTermWeights)
+    longTermTotalWeight = sum(longTermWeights)
+
+    i = 0
+    while i < LongTermLength:
+        if( i < shortTermLength):
+            shortTermWeights[i] = (shortTermWeights[i] / shortTermTotalWeight)
+        longTermWeights[i] = (longTermWeights[i] / longTermTotalWeight)
+        i += 1 
+
+
+    i = LongTermLength
+    prices = data['Price'].to_list()
+
+
+    numUnits = 0
+    while i < len(prices):
+        longMovingAverage = (np.dot(prices[i - LongTermLength:i], longTermWeights))
+        shortMovingAverage = (np.dot(prices[i-shortTermLength:i], shortTermWeights))
+        difference = shortMovingAverage - longMovingAverage
+        if( difference >  0):
+            if(numUnits == 0):
+                numUnits = math.floor(cashAvailable / prices[i])
+                cashAvailable -= numUnits * prices[i]
+        elif( difference <  0 or i == len(prices) - 1):
+            if(numUnits != 0):
+                cashAvailable += numUnits * prices[i]
+                numUnits = 0
+        i += 1
+
+
+
+
+
+    totalReturn = (cashAvailable - workingCapital) / workingCapital
+    bmark = (data['Price'][len(prices) - 1] - data['Price'][LongTermLength]) /  data['Price'][LongTermLength]
+    print('Benchmark return: ' + str(bmark))
+    print('Total return: ' + str(totalReturn))
+
+    if( totalReturn > bmark):
+        print("WE BEAT THE MARKET")
+
+if __name__ == "__main__":
